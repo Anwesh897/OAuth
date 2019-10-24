518099bd055bfc9a3996ab859bc3acc80562d7d7
Anwesh <anwesh897@gmail.com>
2019-10-24 10:40:14 +0530

    commit

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,9 @@ module.exports = {
     clientID:
       "522333698439-qd6uu34c1hcjstasbentgnpimpe7koqe.apps.googleusercontent.com",
     clientSecret: "Qatz3hUhkGzpivhLZad3cXPY"
+  },
+  mongodb: {
+    dbURI:
+      "mongodb+srv://Anwesh:Aa9479041046@cluster0-2wiy3.gcp.mongodb.net/test"
   }
 };
--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const authRoute = require("./routes/oauth-routes");
 const passportSetup = require("./config/passport-setup");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
 const app = express();
 
 ///Set view engine
 app.set("view engine", "ejs");
+
+///connect to mongodb
+mongoose.connect(keys.mongodb.dbURI, () => {
+  console.log("connect to mongodb");
+});
 
 ///Set up routes
 app.use("/auth", authRoute);
