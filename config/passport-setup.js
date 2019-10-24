bf3669d5fbc2f249643707a7ccd017b240378805
Anwesh <anwesh897@gmail.com>
2019-10-24 12:32:04 +0530

    commit

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
 const keys = require("./keys");
+const dotenv = require("dotenv");
 const User = require("../models/user-model");
 
 passport.use(
@@ -12,17 +13,23 @@ passport.use(
       clientSecret: keys.google.clientSecret
     },
     (accessToken, refreshToken, profile, done) => {
-      ///passport call back function
-      console.log("passport call back function fired");
-      console.log(profile);
-      new User({
-        username: profile.displayName,
-        googleId: profile.id
-      })
-        .save()
-        .then(newUser => {
-          console.log(`new user created ${newUser}`);
-        });
+      ///Check if user already exist
+      User.findOne({ googleId: profile.id }).then(currentuser => {
+        if (currentuser) {
+          ///already have a user
+          console.log(`User is ${currentuser}`);
+        } else {
+          ///if not create user
+          new User({
+            username: profile.displayName,
+            googleId: profile.id
+          })
+            .save()
+            .then(newUser => {
+              console.log(`new user created ${newUser}`);
+            });
+        }
+      });
     }
   )
 );
