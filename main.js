9b27d2652cefbee4d1bac1241f5c8cddcc0c1c73
Anwesh <anwesh897@gmail.com>
2019-10-24 18:49:35 +0530

    commit

--- main.js
+++ main.js
@@ -37,4 +37,5 @@ app.get("/", (req, res) => {
   res.render("home", { user: req.user });
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
